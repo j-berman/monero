36c2e8866eb62046d92997658910ed1ab266d76e
0xfffc <0xfffc0000@proton.me>
2025-11-13 17:45:38 +0000

    addressing first round of jberman comments

--- src/cryptonote_protocol/cryptonote_protocol_handler.h
+++ src/cryptonote_protocol/cryptonote_protocol_handler.h
@@ -183,6 +183,7 @@ namespace cryptonote
     bool check_standby_peers();
     bool update_sync_search();
     bool check_txpool_complement();
+    std::mutex m_check_tx_request_queue_mutex;
     bool check_tx_request_queue();
     int try_add_next_blocks(cryptonote_connection_context &context);
     void notify_new_stripe(cryptonote_connection_context &context, uint32_t stripe);
