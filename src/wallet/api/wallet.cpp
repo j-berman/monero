cbd853a9e45fda9455e82107afddb68f475b83a0
j-berman <justinberman@protonmail.com>
2026-06-04 08:57:05 -0700

    Disable SSL in client

--- src/wallet/api/wallet.cpp
+++ src/wallet/api/wallet.cpp
@@ -2518,7 +2518,7 @@ void WalletImpl::pendingTxPostProcess(PendingTransactionImpl * pending)
 bool WalletImpl::doInit(const string &daemon_address, const std::string &proxy_address, uint64_t upper_transaction_size_limit, bool ssl)
 {
     MDEBUG("Calling m_wallet->init");
-    if (!m_wallet->init(daemon_address, m_daemon_login, proxy_address, upper_transaction_size_limit))
+    if (!m_wallet->init(daemon_address, m_daemon_login, proxy_address, upper_transaction_size_limit, true, epee::net_utils::ssl_support_t::e_ssl_support_disabled))
        return false;
     MDEBUG("Finished m_wallet->init");
 
